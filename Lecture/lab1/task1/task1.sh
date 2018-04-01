1cb32c21bc62afb6b79ad623068c4587d57831d4
Oleg Lipskiy <lolego1601@gmail.com>
2018-04-02 00:44:43 +0300

    feat: done lab1 task1

---
+++ Lecture/lab1/task1/task1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SEARCH_PATH=$1
+SUFFIX=$2
+
+if [[ -z $SEARCH_PATH ]]
+then
+echo "Please enter search path"
+exit 1
+elif [[ -z $SUFFIX ]]
+then 
+echo "Please enter search suffix"
+exit 1
+else
+echo "Searching file with suffix '$SUFFIX' in $SEARCH_PATH directory..."
+fi
+
+SEARCH_RESULT=`find $SEARCH_PATH -name "*$SUFFIX" -type f -printf "%s %p\n" | sort -n | cut -d " " -f 2-`
+echo "Search result: $SEARCH_RESULT"
+read -p "Please enter destination filename: " DESTINATION
+echo $DESTINATION
+echo "$SEARCH_RESULT">"$DESTINATION"
