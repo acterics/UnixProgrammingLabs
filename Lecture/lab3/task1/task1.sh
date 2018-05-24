ff1aaa10372a96f6b06fd770b326accf19cba445
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-24 13:23:00 +0300

    fix: fixed script

--- Lecture/lab3/task1/task1.sh
+++ Lecture/lab3/task1/task1.sh
@@ -4,9 +4,5 @@ DATA_FILE='./task1/res/data.txt'
 RESULT_FILE='./task1/res/result.txt'
 
 read -r REGEXP < $REGEXP_FILE
-read -r DATA < $DATA_FILE
-echo $REGEXP
-echo "Data: $DATA"
 
-# echo $(grep -E -o "$REGEXP" $DATA_FILE)
-grep -E -o "$REGEXP" $DATA_FILE >> $RESULT_FILE
+grep -E -o "$REGEXP" $DATA_FILE > $RESULT_FILE
