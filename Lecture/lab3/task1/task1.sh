6e9c1863bc1919e09591565042c5e43d4b449de5
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-24 12:18:16 +0300

    feat: task1 done

--- Lecture/lab3/task1/task1.sh
+++ Lecture/lab3/task1/task1.sh
@@ -3,11 +3,10 @@ REGEXP_FILE='./task1/res/regexp.txt'
 DATA_FILE='./task1/res/data.txt'
 RESULT_FILE='./task1/res/result.txt'
 
-read -r REGEXP < "$REGEXP_FILE"
-
+read -r REGEXP < $REGEXP_FILE
+read -r DATA < $DATA_FILE
 echo $REGEXP
+echo "Data: $DATA"
 
-RESULT=grep -l $REGEXP $DATA_FILE
-
-
-echo $RESULT
+# echo $(grep -E -o "$REGEXP" $DATA_FILE)
+grep -E -o "$REGEXP" $DATA_FILE >> $RESULT_FILE
