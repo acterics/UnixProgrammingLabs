d55e7991abf42cfebdff805dd0450921b790de40
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-24 23:03:16 +0300

    feat: done task

--- Practice/lab2/src/server.py
+++ Practice/lab2/src/server.py
@@ -12,19 +12,53 @@ from generated.protobuf.auth_data_pb2 import (
     UserData, AuthenticationState
 )
 
+root = os.getcwd()
+ips_directory_path = "{root}/res/ips".format(root=root)
+users_directory_path = "{root}/res/users".format(root=root)
 
-class AuthTCPServer(socketserver.TCPServer):
+
+def now():
+    return int(time.time())
+
+
+class AuthTCPServer(socketserver.TCPServer, socketserver.ThreadingMixIn):
 
     def verify_request(self, request, client_address):
         """Blocking banned users by ip address
         """
+        print("Verification")
+        user_ip_path = Path("{ip_path}/{ip_address}.data"
+                            .format(ip_path=ips_directory_path,
+                                    ip_address=client_address[0]))
+        if not user_ip_path.exists():
+            return True
+        user_ip_file = ''
+        try:
+            user_ip_file = user_ip_path.open('rb')
+            user_ip_data = user_ip_file.read()
+            user_ip_file.close()
+            authentication_state = AuthenticationState()
+            authentication_state.ParseFromString(user_ip_data)
+            if authentication_state.ban_expires < now():
+                authentication_state.is_banned = False
+                user_ip_file = user_ip_path.open('wb')
+                user_ip_file.write(authentication_state.SerializeToString())
+                user_ip_file.close()
+            else:
+                sys.stderr.write("BLOCKED: %s'\n" % (client_address[0]))
+                return False
+        except Exception as e:
+            sys.stderr.write("VERIFICATION ERROR: %s - -  %s\n" %
+                             (client_address[0],
+                              "Error: %r" % e))
+            return False
+        finally:
+            if user_ip_file:
+                user_ip_file.close()
         return True
 
 
 class AuthRequestHandler(socketserver.StreamRequestHandler):
-    root = os.getcwd()
-    ips_directory_path = "{root}/res/ips".format(root=root)
-    users_directory_path = "{root}/res/users".format(root=root)
 
     auth_timeout = 300
     ban_timeout = 300
@@ -34,6 +68,8 @@ class AuthRequestHandler(socketserver.StreamRequestHandler):
         self.handle_authorization()
         while not self.close_connection:
             self.handle_message()
+
+        self.log_message("Connection closed")
 
     def handle_authorization(self):
         try:
@@ -70,10 +106,9 @@ class AuthRequestHandler(socketserver.StreamRequestHandler):
 
     def authenticate_user(self, auth_request):
         user_file = ''
-        self.log_message(str(auth_request))
         try:
             user_data_path = Path("{users_path}/{username}.data".format(
-                users_path=self.users_directory_path,
+                users_path=users_directory_path,
                 username=auth_request.username
             ))
             if not user_data_path.exists():
@@ -88,7 +123,7 @@ class AuthRequestHandler(socketserver.StreamRequestHandler):
             user_file.close()
             user_file = user_data_path.open(mode='wb')
             if user_data.password == auth_request.password:
-                user_data.auth_expires = int(time.time()) + self.auth_timeout
+                user_data.auth_expires = now() + self.auth_timeout
                 user_data.tries = 3
                 user_file.write(user_data.SerializeToString())
                 user_file.close()
@@ -118,7 +153,20 @@ class AuthRequestHandler(socketserver.StreamRequestHandler):
                 user_file.close()
 
     def ban_user(self, auth_request):
-        pass
+        authentication_state = AuthenticationState()
+        authentication_state.is_banned = True
+        authentication_state.ban_expires = now() + self.ban_timeout
+        user_ip_file = ''
+        try:
+            user_ip_file = open("{ip_path}/{ip_address}.data"
+                                .format(ip_path=ips_directory_path,
+                                        ip_address=self.client_address[0]), 'wb')
+            user_ip_file.write(authentication_state.SerializeToString())
+        except Exception as e:
+            self.log_error("Ban error %r", e)
+        finally:
+            if user_ip_file:
+                user_ip_file.close()
 
 
 PORT = 8000
